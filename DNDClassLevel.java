e4a651d4b71efaa1f0b66fe25cd31fe1da70ba37
jpayton2916 <152519968+jpayton2916@users.noreply.github.com>
2023-12-12 05:56:03 -0500

    Add files via upload

---
+++ DNDClassLevel.java
@@ -0,0 +1,35 @@
+/**
+JessePayton 
+Class level for GSOn
+*/
+public class DNDClassLevel 
+{
+    private int profBonus;
+
+    /**
+     a DNDClassLevel object
+    */
+    public DNDClassLevel() 
+    {
+
+    }
+
+    /**
+    @return The proficiency bonus
+    */
+    public int getProfBonus() 
+    {
+        return profBonus;
+    }
+
+    /**
+    Sets the proficency bonus for the class level
+    @param profBonus The proficiency bonus to be set
+    */
+    public void setProfBonus(int profBonus) 
+    {
+        this.profBonus = profBonus;
+    }
+
+    
+}
